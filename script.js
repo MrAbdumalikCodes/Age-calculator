04b938c02fd53737a24b36c4ca7701ab6f1c1430
MrAbdumalik <abdumalikcodes7753@gmail.com>
2023-07-03 20:39:58 +0500

    Age-Calculator

---
+++ script.js
@@ -0,0 +1,97 @@
+const output_year = document.querySelector(".output-year")
+const output_month = document.querySelector(".output-month")
+const output_day = document.querySelector(".output-day")
+const submit_btn = document.querySelector(".submit-button")
+// INPUT ELEMENTS
+let isValid = false
+const  input_year = document.querySelector("#year")
+const  input_month = document.querySelector("#month")
+const input_day = document.querySelector("#day")
+// const submit_btn = document.querySelector(".submit-btn")
+
+//ERROR ELEMENTS
+
+const error_year = document.querySelector(".error-year")
+const error_month = document.querySelector(".error-month")
+const error_day = document.querySelector(".error-day")
+
+submit_btn.addEventListener('click', CalculateDate)
+
+input_day.addEventListener('input', e =>{
+    if(+input_day.value > 31){
+        error_day.textContent = 'Must be a valid date'
+        isValid = false;
+        return;
+    }else{
+        isValid = true;
+        error_day.textContent=''
+
+    }
+    if(+input_day.value === 0){
+        isValid = false
+        error_day.textContent = 'The field is required'
+        isValid = false
+        return
+    }else{
+        error_day.textContent = ''
+    }
+})
+
+input_month.addEventListener('input', e =>{
+    if(+input_month.value > 12){
+        error_month.textContent = 'Must be a valid date'
+        isValid = false;
+        return;
+    }else{
+        isValid = true;
+        error_month.textContent=''
+
+    }
+    if(+input_month.value === 0){
+        isValid = false
+        error_month.textContent = 'The field is required'
+        isValid = false
+        return
+    }else{
+        error_month.textContent = ''
+    }
+})
+
+input_year.addEventListener('input', e =>{
+    if(+input_year.value > 2023){
+        error_year.textContent = 'Must be a valid date'
+        isValid = false;
+        return;
+    }else{
+        isValid = true;
+        error_year.textContent=''
+    }
+    if(+input_year.value === 0){
+        isValid = false
+        error_year.textContent = 'The field is required'
+        isValid = false
+        return;
+    }else{
+        error_year.textContent = ''
+    }
+})
+
+function CalculateDate(){
+    if(isValid){
+        let birthday = `${input_month.value}/${input_day.value}/${input_year.value}`
+        console.log(birthday);
+        let birthdayObj = new Date(birthday);
+        let ageDiffMill = Date.now()-birthdayObj;
+        let ageDate = new Date(ageDiffMill);
+        let ageYears = ageDate.getUTCFullYear()-1970;
+        let ageMonth = ageDate.getUTCMonth();
+        let ageDay = ageDate.getUTCDay();
+        console.log(ageDiffMill);
+
+        output_day.textContent = ageDay;
+        output_month.textContent = ageMonth;
+        output_year.textContent = ageYears;
+    }else{
+        alert("error")
+    }
+}
